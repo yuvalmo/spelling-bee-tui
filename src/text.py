0c845153dbe5d6f659bcd452654c478e732f1767
yuvalmo <yuvalmoalem24@gmail.com>
2023-07-18 16:40:46 +0300

    Improve highlight function.

--- src/text.py
+++ src/text.py
@@ -1,11 +1,14 @@
 import re
-
+from typing import Optional
 from termcolor import colored
 
 
-def highlight(text: str, substr: str) -> str:
+def highlight(text: str,
+              substr: Optional[str] = None) -> str:
     ''' Highlight the given substring in the given text.
+    If no substring is given, highlight the whole text.
     '''
+    substr = substr or text
     return re.sub(substr, colored(substr, "yellow"), text)
 
 
